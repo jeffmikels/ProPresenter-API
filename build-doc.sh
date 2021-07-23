60260ce20f291c1663181225da9425f81cd415eb
Jeff Mikels <jeffweb@mikels.cc>
2021-07-23 14:05:11 -0400

    added docs folder

--- build-doc.sh
+++ build-doc.sh
@@ -5,13 +5,8 @@ TARGET=asyncapi.yaml
 
 for i in Pro6 Pro7
 do
-	if [ $i == 'Pro7' ]
-	then
-		exit
-	fi
-
 	./yaml2asyncapi $i/$SOURCE $i/$TARGET
 	# npm install -g @asyncapi/html-template@0.23.0
 	# npm install -g @asyncapi/generator
-	ag $i/$TARGET @asyncapi/html-template@0.23.1 -o $i/docs --force-write -p singleFile=true # -p outFilename=pro6-api.html
+	ag $i/$TARGET @asyncapi/html-template@0.23.1 -o ./docs/$i --force-write -p singleFile=true # -p outFilename=pro6-api.html
 done
