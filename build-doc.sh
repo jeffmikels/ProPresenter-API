eff615835b5585dcdfbda2313aaefe7de9539364
Jeff Mikels <jeffweb@mikels.cc>
2021-07-23 17:14:23 -0400

    improving message sorting in the sidebar

--- build-doc.sh
+++ build-doc.sh
@@ -8,5 +8,5 @@ do
 	./yaml2asyncapi $i/$SOURCE $i/$TARGET
 	# npm install -g @asyncapi/html-template@0.23.0
 	# npm install -g @asyncapi/generator
-	ag $i/$TARGET @asyncapi/html-template@0.23.1 -o ./docs/$i --force-write -p singleFile=true # -p outFilename=pro6-api.html
+	ag $i/$TARGET @asyncapi/html-template@0.23.4 -o ./docs/$i --force-write -p singleFile=true # -p outFilename=pro6-api.html
 done
